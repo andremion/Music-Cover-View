5e2f1d0705331363af367a6542e487295c9d849b
André Mion <andremion@gmail.com>
2016-08-25 12:23:09 +0100

    Update README file and fix copyright headers

--- library/src/main/java/com/andremion/music/MusicCoverViewTransition.java
+++ library/src/main/java/com/andremion/music/MusicCoverViewTransition.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016. André Mion
+ * Copyright (c) 2016 André Mion
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
